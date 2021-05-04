7469682b1152423f61a6c4634554af8cc1c8b1f4
Hubert <lewandowski.hubert486@gmail.com>
2021-05-04 19:42:56 +0200

    Documentation of code

--- wikiparser/__init__.py
+++ wikiparser/__init__.py
@@ -2,4 +2,4 @@ import wikicrawler
 
 if __name__ == "__main__":
     crawler = wikicrawler.WikiCrawler()
-    
+
--- wikiparser/wikicrawler/__init__.py
+++ wikiparser/wikicrawler/__init__.py
@@ -5,6 +5,7 @@ from bs4 import BeautifulSoup
 class WikiCrawler:
     @staticmethod
     def __site_exist(url: str):
+        '''Verifies if given URL is valid'''
         request = requests.get(url)
 
         if request.status_code == 200:
@@ -14,17 +15,19 @@ class WikiCrawler:
         return False
 
     @staticmethod
-    def __get_url_to_raw(url: str):
-        modified = url.replace('https://', '')
-        modified = modified.replace('http://', '')
+    def __get_url_of_wikilang(url: str):
+        '''Returns URL to page containing WikiLang code'''
+        wikilang_url = url.replace('https://', '')
+        wikilang_url = wikilang_url.replace('http://', '')
 
-        root = modified.split('/')[0]
-        title = modified.split('/')[-1]
-        modified = "https://{0}/w/index.php?title={1}&action=edit".format(root, title)
+        root = wikilang_url.split('/')[0]
+        title = wikilang_url.split('/')[-1]
+        wikilang_url = "https://{0}/w/index.php?title={1}&action=edit".format(root, title)
 
-        return modified
+        return wikilang_url
 
     def get_page(self, url: str):
+        '''Returns HTML & WikiLang of given site'''
         if not self.__site_exist(url):
             return None, None
 
@@ -33,13 +36,14 @@ class WikiCrawler:
         html = html.find(id="bodyContent")
         html = html.find(id="mw-content-text")
 
-        raw_response = requests.get(self.__get_url_to_raw(url))
-        raw = BeautifulSoup(raw_response.content, 'html.parser')
-        raw = raw.find(id="wpTextbox1")
+        wikilang_response = requests.get(self.__get_url_of_wikilang(url))
+        wikilang = BeautifulSoup(wikilang_response.content, 'html.parser')
+        wikilang = wikilang.find(id="wpTextbox1")
 
-        return html, raw
+        return { "html" : html, "wikilang" : wikilang }
 
     def get_pages(self, urls: list):
+        '''Returns HTML & WikiLang of given sites'''
         out = []
 
         for url in urls:
@@ -51,11 +55,13 @@ class WikiCrawler:
         return out
 
     def get_random_page(self):
+        '''Generates random wikipedia page and returns HTML & WikiLang of site'''
         request = requests.get("https://en.wikipedia.org/wiki/Special:Random")
 
         return self.get_page(request.url)
 
     def get_random_pages(self, count: int):
+        '''Generates N random wikipedia pages and returns HTML & WikiLang of sites'''
         out = []
 
         for i in range(count):
